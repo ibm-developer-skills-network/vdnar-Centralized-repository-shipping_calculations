6af7a7cbe93a4eb600589114941daf7c3cb1856c
Vladimir Paredes <vladimirparedesa@gmail.com>
2024-03-28 13:51:49 -0400

    Revert "added aditional line to Shipping Cost Calulator"
    This reverts commit 2a26a60ba56adc9c1b6dfd398a4e0e95e448d3f9.

--- Shipping_Cost_Calculator.py
+++ Shipping_Cost_Calculator.py
@@ -2,8 +2,6 @@
 
  # Here is a new update by VladimirParedes
 
- # Here is another update by Vladimir Paredes
-
 ## Input package weight and shipping rate
 weight = float(input("Enter the package weight in kilograms: "))
 rate = float(input("Enter the shipping rate per kilogram: "))

fb34e81a9dc4a4aed3bc20f0ebe792e36793add7
othmane el hour <elhourothmane@gmail.com>
2024-04-08 16:47:59 -0400

    Revert "added additional line to Shipping_Cost_Calculator.py"
    This reverts commit 56b9a54b420e49467db7b672e674690d7650d460.

--- Shipping_Cost_Calculator.py
+++ Shipping_Cost_Calculator.py
@@ -10,5 +10,4 @@ shipping_cost = weight * rate
 ## Display the result
 print(f"Shipping Cost: {shipping_cost} USD")
 
- # Here is a new update by otayou
-# Here is another update by otayou
+ # Here is a new update by <your GitHub username>

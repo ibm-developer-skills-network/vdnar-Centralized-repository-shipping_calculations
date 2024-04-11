33b00ecc913b09657ba13f172e6f1aae3cec8143
YohansTgeb <yoni.zone@gmail.com>
2024-04-11 07:57:47 -0400

    added additional line to Shipping_Cost_Calculator.py

--- Shipping_Cost_Calculator.py
+++ Shipping_Cost_Calculator.py
@@ -10,3 +10,4 @@ shipping_cost = weight * rate
 ## Display the result
 print(f"Shipping Cost: {shipping_cost} USD")
  # Here is a new update by YohansTgeb on 11.04.2024
+ # Here is another update by YohansTgeb on 11.04.2024

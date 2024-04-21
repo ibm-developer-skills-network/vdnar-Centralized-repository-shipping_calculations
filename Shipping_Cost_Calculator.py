327016bda91f7236f47074c4b17a7bc1873ed3a7
arekmurzyn <arek.murzyn@gmail.com>
2024-04-21 05:43:20 -0400

    Revert "another new line in python file"
    This reverts commit e199f1ef738fabf3b91ef3ba1ad1a09b5ce6e9d4.

--- Shipping_Cost_Calculator.py
+++ Shipping_Cost_Calculator.py
@@ -1,6 +1,5 @@
 # Shipping Cost Calculator
 # Here is a new update by arekmurzyn
-# Here is a another new line
 ## Input package weight and shipping rate
 weight = float(input("Enter the package weight in kilograms: "))
 rate = float(input("Enter the shipping rate per kilogram: "))

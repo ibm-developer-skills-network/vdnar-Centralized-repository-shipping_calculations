fe212a938e6b2c9669bd2c70ccb8690d62465b3d
ValeriaNevarez <mvno.071095@hotmail.com>
2024-04-26 17:29:05 -0400

    Revert "added another update to Shipping_Cost_Calculator"
    This reverts commit e1dc4502fa1ea763c9fef3efebf92b00b0c96934.

--- Shipping_Cost_Calculator.py
+++ Shipping_Cost_Calculator.py
@@ -1,6 +1,5 @@
 # Shipping Cost Calculator
 # Here is a new update by ValeriaNevarez
-# Here is another update by ValeriaNevarez
 
 ## Input package weight and shipping rate
 weight = float(input("Enter the package weight in kilograms: "))

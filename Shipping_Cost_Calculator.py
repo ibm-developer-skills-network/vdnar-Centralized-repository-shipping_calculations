8aae314431f4e6c065e37b49f843b6399f9d705d
sridhar-e-it <srid.71772118145@gct.ac.in>
2024-05-01 03:38:02 -0400

    added additional line to Shipping_Cost_Calculator.py

--- Shipping_Cost_Calculator.py
+++ Shipping_Cost_Calculator.py
@@ -10,3 +10,4 @@ shipping_cost = weight * rate
 ## Display the result
 print(f"Shipping Cost: {shipping_cost} USD")
  # Here is a new update by sridhar-e-it
+ # Here is another update by sridhar-e-it

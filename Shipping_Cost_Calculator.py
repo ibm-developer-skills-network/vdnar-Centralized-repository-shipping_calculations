fb5cfb4bb576f7b71b819a2d3aa053e949485df2
RAJESWARI-P19 <raje.71772117134@gct.ac.in>
2024-05-08 09:32:07 +0530

    changed

--- Shipping_Cost_Calculator.py
+++ Shipping_Cost_Calculator.py
@@ -5,8 +5,8 @@ weight = float(input("Enter the package weight in kilograms: "))
 rate = float(input("Enter the shipping rate per kilogram: "))
 
 ## Calculate shipping cost
-shipping_cost = weight * rate
+ship_cost = weight * rate
 
 ## Display the result
-print(f"Shipping Cost: {shipping_cost} USD")
+print(f"Shipping Cost: {ship_cost} USD")
 

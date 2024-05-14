867e6ea06925bc7359ee01af67e3e71a42bc9b20
saraabbas-saal <sara.abbas@saal.ai>
2024-05-14 04:19:04 -0400

    adding new comment to branch 2

--- Shipping_Cost_Calculator.py
+++ Shipping_Cost_Calculator.py
@@ -1,4 +1,5 @@
-#  # Here is a new update by saraabbas-saal
+# Here is another update by saraabbas-saal
+# Here is a new update by saraabbas-saal
 # Shipping Cost Calculator
 
 ## Input package weight and shipping rate

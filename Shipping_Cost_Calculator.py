b59c8ccf617f0fa0daed2db29a29e12869f3731c
chaitenyachand <129392157+chaitenyachand@users.noreply.github.com>
2024-05-21 21:03:00 +0530

    Update Shipping_Cost_Calculator.py

--- Shipping_Cost_Calculator.py
+++ Shipping_Cost_Calculator.py
@@ -10,3 +10,8 @@ shipping_cost = weight * rate
 ## Display the result
 print(f"Shipping Cost: {shipping_cost} USD")
  # Here is a new update by chaitenychand
+git config --global user.email chaitenyachand@gmail.com
+git config --global user.name chaitenyachand
+git add .
+git commit -m "added a new line to Shipping_Cost_Calculator.py"
+git status

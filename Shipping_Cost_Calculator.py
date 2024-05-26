7174bb804fb4cd918e49ebbff3081a780e3c411e
ronaldorcosta <ronaldorcosta@gmail.com>
2024-05-26 19:40:47 -0400

    teste ronaldo

--- Shipping_Cost_Calculator.py
+++ Shipping_Cost_Calculator.py
@@ -1,5 +1,7 @@
 # Shipping Cost Calculator
 
+# inseri linha ronaldo
+
 ## Input package weight and shipping rate
 weight = float(input("Enter the package weight in kilograms: "))
 rate = float(input("Enter the shipping rate per kilogram: "))

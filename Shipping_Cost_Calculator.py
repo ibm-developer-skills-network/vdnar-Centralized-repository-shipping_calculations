f25a83527aebfd077ea99d31a218141eec4b5bbe
Mathewos Beyene <mathewosbeyene5@gmail.com>
2024-05-29 16:44:38 -0400

    shipping cost modified

--- Shipping_Cost_Calculator.py
+++ Shipping_Cost_Calculator.py
@@ -6,6 +6,8 @@ rate = float(input("Enter the shipping rate per kilogram: "))
 
 ## Calculate shipping cost
 shipping_cost = weight * rate
+tax = shipping_cost*13/100
+shipping_cost += tax
 
 ## Display the result
 print(f"Shipping Cost: {shipping_cost} USD")

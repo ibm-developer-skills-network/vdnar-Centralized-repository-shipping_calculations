3e983f68789a2c3b33a67c08e460ee68464c221a
Ahmad Hatami <hhatamdev@outlook.com>
2024-05-30 13:08:55 -0400

    Revert "added another update"
    This reverts commit 3073df2cb9956ff87d6597cefea8fb71f0c9ade7.

--- Shipping_Cost_Calculator.py
+++ Shipping_Cost_Calculator.py
@@ -11,4 +11,3 @@ shipping_cost = weight * rate
 print(f"Shipping Cost: {shipping_cost} USD")
 
  # Here is a new update by <hhatamdev@outlook.com>
- # Here is another update by <hhatamdev@outlook.com>

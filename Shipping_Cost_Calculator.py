5bbfbbead8886a292cfcbadbf796ae26fed861bc
Muhannad Al-Mozaiger <programmer22muhannad@gmail.com>
2024-06-01 00:17:29 +0300

    Revert "added additional line to Shipping_Cost_Calculator.py"
    This reverts commit 1ddca5f14e2fa9dd9f3977c8ed89da319b9674c4.

--- Shipping_Cost_Calculator.py
+++ Shipping_Cost_Calculator.py
@@ -10,4 +10,3 @@ shipping_cost = weight * rate
 ## Display the result
 print(f"Shipping Cost: {shipping_cost} USD")
 # Here is a new update by Muhannad-Al-Mozaiger
-# Here is another update by Muhannad-Al-Mozaiger

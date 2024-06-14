1e403d081c2d70c58117595b787cd35bea72ffe8
CristeaStefan <100153523+CristeaStefan@users.noreply.github.com>
2024-06-14 14:13:40 +0300

    Update Shipping_Cost_Calculator.py

--- Shipping_Cost_Calculator.py
+++ Shipping_Cost_Calculator.py
@@ -1,3 +1,5 @@
+Insert the following line into the Shipping_Cost_Calculator.py file, replacing <your GitHub username> with your real GitHub username.
+
 # Shipping Cost Calculator
 
 ## Input package weight and shipping rate

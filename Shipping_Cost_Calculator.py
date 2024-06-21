d3c0d4df0944775b4615ac4547ab63e5718d5067
Nikhil Sahasrabudhe <nikhil.sahasrabudhe@synacor.com>
2024-06-21 17:56:10 +0530

    INR conversion added by Nikhil Sahasrabudhe

--- Shipping_Cost_Calculator.py
+++ Shipping_Cost_Calculator.py
@@ -3,10 +3,13 @@
 ## Input package weight and shipping rate
 weight = float(input("Enter the package weight in kilograms: "))
 rate = float(input("Enter the shipping rate per kilogram: "))
+inr_exch_rate = float(input("Enter Dollar to INR Exchange rate for today: ")
 
 ## Calculate shipping cost
 shipping_cost = weight * rate
+cost_in_inr = shipping_cost * inr_exch_rate
 
 ## Display the result
 print(f"Shipping Cost: {shipping_cost} USD")
+print(f"Shipping Cost in INR Today: {cost_in_inr} Rupee")
 

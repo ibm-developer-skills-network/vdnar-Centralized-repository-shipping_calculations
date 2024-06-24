09c9a7fc1bcf1d259ab5a2c15a3c7ac5c81c9f43
marcelo-crisostomo <marcelocrisostomodeveloper@gmail.com>
2024-06-24 16:10:47 -0400

    nueva rama

--- Shipping_Cost_Calculator.py
+++ Shipping_Cost_Calculator.py
@@ -1,11 +1,11 @@
 # Shipping Cost Calculator
 
 ## Input package weight and shipping rate
-weight = float(input("Ingresa el peso del paquete en kilogramos: "))
-rate = float(input("Ingresa la tasa de envío según el perso en kilogramos"))
+peso = float(input("Ingresa el peso del paquete en kilogramos: "))
+tasa = float(input("Ingresa la tasa de envío según el perso en kilogramos"))
 
 ## Calculate shipping cost
-shipping_cost = weight * rate
+shipping_cost = peso * tasa
 
 ## Display the result
 print(f"El costo de envío es : {shipping_cost} CLP")

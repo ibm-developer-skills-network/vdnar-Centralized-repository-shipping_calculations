80333325faa72c5f93d83026a09935a0be48e6b9
Essadike  Elhafiane <eelhafia@e3r5p1.1337.ma>
2024-06-25 20:52:25 +0100

    added additional line to Shipping_Cost_Calculator.py

--- Shipping_Cost_Calculator.py
+++ Shipping_Cost_Calculator.py
@@ -1,4 +1,5 @@
- # Here is a new update by essadike-elhafiane
+# Here is another update by essadike-elhafiane
+# Here is a new update by essadike-elhafiane
 # Shipping Cost Calculator
 
 ## Input package weight and shipping rate

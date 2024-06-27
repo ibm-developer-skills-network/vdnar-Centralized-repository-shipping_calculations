7ad7da442c911b60820b66d44f2b7479ae215834
rdww60 <brpage@austin.rr.com>
2024-06-27 17:44:47 -0400

    Revert "another update"
    This reverts commit 3e593431b7c7ce5a8b0c40d427884f935ff31752.

--- Shipping_Cost_Calculator.py
+++ Shipping_Cost_Calculator.py
@@ -1,5 +1,4 @@
 # Here is a new update by rdww60
-#Here is another update by rdww60
 # Shipping Cost Calculator
 
 ## Input package weight and shipping rate
@@ -11,5 +10,4 @@ shipping_cost = weight * rate
 
 ## Display the result
 print(f"Shipping Cost: {shipping_cost} USD")
-print("Crazy!!")
 

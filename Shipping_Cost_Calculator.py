e2dbad3eb67002e2c2bfec0ac3676d78ae1c06e0
Ruba <ruba.soboh@masterelectronics.com>
2024-07-05 12:58:53 -0700

    Revert "Commiting the final changes"
    This reverts commit b1a6abc810d49d3dd7e1c0f1953683226804ac17.

--- Shipping_Cost_Calculator.py
+++ Shipping_Cost_Calculator.py
@@ -11,4 +11,3 @@ shipping_cost = weight * rate
 print(f"Shipping Cost: {shipping_cost} USD")
 
 "# Here is another update by ruba-master, but this one should be reverted." 
-"# Here is another update by ruba-master, but this one should be reverted." 

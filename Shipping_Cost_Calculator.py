b1a6abc810d49d3dd7e1c0f1953683226804ac17
Ruba <ruba.soboh@masterelectronics.com>
2024-07-05 12:58:12 -0700

    Commiting the final changes

--- Shipping_Cost_Calculator.py
+++ Shipping_Cost_Calculator.py
@@ -11,3 +11,4 @@ shipping_cost = weight * rate
 print(f"Shipping Cost: {shipping_cost} USD")
 
 "# Here is another update by ruba-master, but this one should be reverted." 
+"# Here is another update by ruba-master, but this one should be reverted." 

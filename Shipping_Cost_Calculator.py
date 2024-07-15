753853583a14b1e8a0d9ee1b0cf06fe2b9aaa585
paije-hub <paijemcgrath@gmail.com>
2024-07-15 14:47:29 -0600

    Update Shipping_Cost_Calculator.py

--- Shipping_Cost_Calculator.py
+++ Shipping_Cost_Calculator.py
@@ -10,3 +10,14 @@ shipping_cost = weight * rate
 ## Display the result
 print(f"Shipping Cost: {shipping_cost} USD")
 
+# Here is another update by paije-hub
+git add .
+git commit -m "added additional line to Shipping_Cost_Calculator.py"
+  git revert HEAD --no-edit
+ # Here is a new update by paije-hub
+ git config --global user.email "paijemcgrath@gmail.com"
+ git config --global user.name "paije-hub"
+ git add .
+git commit -m "added a new line to Shipping_Cost_Calculator.py"
+git status
+git push --set-upstream origin shipping_calculator_fixes

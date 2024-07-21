c6f59c04887c1fbb53b8df9168d0fc2de82a0a5b
elazrev <elaz.rev@gmail.com>
2024-07-21 17:01:09 -0400

    add new line to Sipping_Cost_Calculator.py

--- Shipping_Cost_Calculator.py
+++ Shipping_Cost_Calculator.py
@@ -32,3 +32,4 @@ while currency not in ["USD", "ILS", "EUR"]:
 print(f"Shipping Cost: {exchanger.exchanger(shipping_cost, currency)} {currency}")
 
 # Here is another update by elazrev #
+print("Thank you!")

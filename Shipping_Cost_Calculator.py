2a20dad59dfad588eaf6b1104a3256d6f42762aa
Suk Hyun Suh <shsuh1999@gmail.com>
2024-07-27 03:43:37 -0400

    added additional line to Shipping_Cost_Calculator.py

--- Shipping_Cost_Calculator.py
+++ Shipping_Cost_Calculator.py
@@ -1,5 +1,6 @@
 # Shipping Cost Calculator
-# Here is a new update by <your GitHub username>
+# Here is a new update by shsuh1999
+# Here is another update by shsuh1999
 
 ## Input package weight and shipping rate
 weight = float(input("Enter the package weight in kilograms: "))

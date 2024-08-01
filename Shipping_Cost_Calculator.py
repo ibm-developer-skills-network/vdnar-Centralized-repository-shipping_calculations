d17e83957f122fff934094b3054ac82fffe401e3
Ussaid Imtiaz <imtiazussaid@gmail.com>
2024-08-01 12:28:46 +0500

    added tax with shipping cost

--- Shipping_Cost_Calculator.py
+++ Shipping_Cost_Calculator.py
@@ -4,9 +4,15 @@
 weight = float(input("Enter the package weight in kilograms: "))
 rate = float(input("Enter the shipping rate per kilogram: "))
 
+
 ## Calculate shipping cost
 shipping_cost = weight * rate
+rate_of_tax = 0.05
+tax = rate_of_tax * shipping_cost
+shipping_cost_incl_tax = shipping_cost + tax
 
 ## Display the result
-print(f"Shipping Cost: {shipping_cost} USD")
+print(f"Shipping Cost Including Tax: {shipping_cost} USD + {tax} USD = {shipping_cost_incl_tax} USD")
+
+
 

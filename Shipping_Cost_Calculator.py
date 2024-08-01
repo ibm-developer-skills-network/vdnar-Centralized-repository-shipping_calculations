8edbd4c4683d9a2c3e3b2930619ac6b3269d10fc
Mhossni <mohamed.hossni.hashem@gmail.com>
2024-08-01 18:54:29 -0400

    Update Shipping_Cost_Calculator.py

--- Shipping_Cost_Calculator.py
+++ Shipping_Cost_Calculator.py
@@ -7,6 +7,9 @@ rate = float(input("Enter the shipping rate per kilogram: "))
 ## Calculate shipping cost
 shipping_cost = weight * rate
 
+#converting rates
+print(f"Shipping Cost: {shipping_cost * 3.76} AED")
+
 ## Display the result
 print(f"Shipping Cost: {shipping_cost} USD")
 

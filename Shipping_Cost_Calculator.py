fbd414b79dfd5a2ce600cb7e7964564cbd0a46d9
SunilGodara29 <hurryupgodara@gmail.com>
2024-08-03 06:54:05 -0400

    commented correctly in py file

--- Shipping_Cost_Calculator.py
+++ Shipping_Cost_Calculator.py
@@ -11,5 +11,5 @@ shipping_cost = weight * rate
 print(f"Shipping Cost: {shipping_cost} USD")
 
  # Here is a new update by SunilGodara29
- # Here is another update by SunilGodara29
+ # Change made in branch fix
 

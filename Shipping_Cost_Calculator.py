98818c9737720204cda8ef77cf0b0762cd05f5eb
Khoa <anhkhoa1597@yahoo.com.vn>
2024-08-12 04:30:23 -0400

    added a new line to Shipping_Cost_Calculator.py

--- Shipping_Cost_Calculator.py
+++ Shipping_Cost_Calculator.py
@@ -1,3 +1,4 @@
+# Here is a new update by anhkhoa1597
 # Shipping Cost Calculator
 
 ## Input package weight and shipping rate
@@ -9,4 +10,3 @@ shipping_cost = weight * rate
 
 ## Display the result
 print(f"Shipping Cost: {shipping_cost} USD")
-

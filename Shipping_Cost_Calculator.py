d366051e744ff2879ca31daacad2c6a486bf00c4
Alienz11 <nsoforkenechukwu2@gmail.com>
2024-08-25 11:35:23 -0400

    added additional line to Shipping_Cost_Calculator.py

--- Shipping_Cost_Calculator.py
+++ Shipping_Cost_Calculator.py
@@ -1,4 +1,5 @@
- # Here is a new update by Alienz11
+# Here is a new update by Alienz11
+# Here is another update by Alienz11
 # Shipping Cost Calculator
 
 ## Input package weight and shipping rate

5db3a88af3d2743df075ca56215c7d9250e4ae0c
selvajayanth <selvakumarjayanth@gmail.com>
2024-08-29 01:35:39 -0400

    added new line second time to correct the mistake

--- Shipping_Cost_Calculator.py
+++ Shipping_Cost_Calculator.py
@@ -10,4 +10,4 @@ shipping_cost = weight * rate
 ## Display the result
 print(f"Shipping Cost: {shipping_cost} USD")
 
- # Here is a new update by Selvajayanth
+ # Here is a new update by Selvajayanth 29-08-2024

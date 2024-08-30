484fe772b40c9caa81628c6991455d3988331a53
Zai Yang <yangz112504@gmail.com>
2024-08-29 22:44:09 -0400

    added one more update

--- Shipping_Cost_Calculator.py
+++ Shipping_Cost_Calculator.py
@@ -11,5 +11,7 @@ shipping_cost = weight * rate
 print(f"Shipping Cost: {shipping_cost} USD")
 
  # Here is a new update by <yangz112504@gmail.com>
+ # Here is another update by yangz112504@gmail.com
+
 
 

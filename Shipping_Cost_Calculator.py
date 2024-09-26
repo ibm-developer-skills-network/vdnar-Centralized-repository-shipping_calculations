d7a0e10f7f697c9c64b69fb1c909ef00cc08a65c
JessicaALeite <jessica.leite2303@gmail.com>
2024-09-26 16:16:33 -0300

    Added  text to Shipping_cost _calculator.py

--- Shipping_Cost_Calculator.py
+++ Shipping_Cost_Calculator.py
@@ -1,3 +1,5 @@
+# Here is a new update by JessicaALeite
+# Here is another update by JessicaALeite
 # Shipping Cost Calculator
 
 ## Input package weight and shipping rate
@@ -10,6 +12,3 @@ shipping_cost = weight * rate
 ## Display the result
 print(f"Shipping Cost: {shipping_cost} USD")
 
-# File added by Sklup55
-
- # Here is a new update by Sklup55

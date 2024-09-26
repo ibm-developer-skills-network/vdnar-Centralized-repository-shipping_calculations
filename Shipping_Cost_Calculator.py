362b98595b1827b68ce2fa9b1170f0bcef3f8839
jamellemessinger <jamswoof914@gmail.com>
2024-09-26 13:07:05 -0400

    Added another updated

--- Shipping_Cost_Calculator.py
+++ Shipping_Cost_Calculator.py
@@ -1,4 +1,5 @@
- # Here is a new update by jamellemessinger
+# Here is a new update by jamellemessinger
+# Here is another update by jamellemessinger
 # Shipping Cost Calculator
 
 ## Input package weight and shipping rate

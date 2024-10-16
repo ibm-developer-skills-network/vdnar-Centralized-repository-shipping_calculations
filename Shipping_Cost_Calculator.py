2aafa11508d9177568213d14ee9355722f0636f9
Archana <archananp1991@gmail.com>
2024-10-15 23:45:09 -0400

    Revert "Added another comment"
    This reverts commit b354a0dfa4743a9c7aaa2f12651184750ce2785d.

--- Shipping_Cost_Calculator.py
+++ Shipping_Cost_Calculator.py
@@ -1,6 +1,5 @@
 # Shipping Cost Calculator
 # Here is a new update by Archana
-# Here is another update by Karthick
 
 ## Input package weight and shipping rate
 weight = float(input("Enter the package weight in kilograms: "))

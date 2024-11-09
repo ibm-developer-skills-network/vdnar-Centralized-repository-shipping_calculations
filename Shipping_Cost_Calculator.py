f008f61e180ea634ca0413d2531a9bb774198843
KrishnaAher <kishnaaher68.com>
2024-11-09 11:46:37 +0530

    Revert "Saving current changes before revert"
    This reverts commit 53e17bae8a71ee61dfa90f792389c9ca4113f95d.

--- Shipping_Cost_Calculator.py
+++ Shipping_Cost_Calculator.py
@@ -1,12 +1,12 @@
 # Shipping Cost Calculator
-# Here is a new update by KrishnaAher
-# Here is another update by KrihnaAher
-# Input package weight and shipping rate
+ # Here is a new update by <KrishnaAher>
+## Input package weight and shipping rate
 weight = float(input("Enter the package weight in kilograms: "))
 rate = float(input("Enter the shipping rate per kilogram: "))
 
-# Calculate shipping cost
+## Calculate shipping cost
 shipping_cost = weight * rate
 
-# Display the result
+## Display the result
 print(f"Shipping Cost: {shipping_cost} USD")
+

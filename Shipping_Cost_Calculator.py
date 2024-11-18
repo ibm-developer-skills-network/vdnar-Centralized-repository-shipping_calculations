44672ba5718c4df549e1a1c53c864b7ce34bf30c
Hemalatha-gv <hemagvdec25@gmail.com>
2024-11-17 20:15:06 -0600

    Added changes to shipping cost calculator.py

--- Shipping_Cost_Calculator.py
+++ Shipping_Cost_Calculator.py
@@ -1,6 +1,8 @@
 # Here is a new update by Hemalatha-GV
 weight = float(input("Enter the package weight in kilograms: "))
 rate = float(input("Enter the shipping rate per kilogram: "))
+
 shipping_cost = weight * rate 
+#display
 print(f"Shipping Cost: {shipping_cost} USD")
 

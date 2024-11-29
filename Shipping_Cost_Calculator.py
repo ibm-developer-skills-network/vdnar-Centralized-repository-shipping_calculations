94f1550c701994aee302250653731a2385e67ff3
PPH <pph.mgy@gmail.com>
2024-11-29 10:25:52 -0500

    Revert "add another update name in pythonfile"
    This reverts commit f5364a118a153f0075b5df06c6e752b1ad57f860.

--- Shipping_Cost_Calculator.py
+++ Shipping_Cost_Calculator.py
@@ -1,6 +1,5 @@
 # Shipping Cost Calculator
 # Here is a new update by PyaePhyo777
-# Here is another update by PyaePhyo777
 ## Input package weight and shipping rate
 weight = float(input("Enter the package weight in kilograms: "))
 rate = float(input("Enter the shipping rate per kilogram: "))

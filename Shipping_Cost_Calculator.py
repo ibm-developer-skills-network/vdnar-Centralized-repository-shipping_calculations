570c3be95d6c283e2b74de6e3fe806b8766e2261
tony-s-h <tony.s.hancock@gmail.com>
2024-12-16 06:46:28 -0500

    Revert "added another new line"
    This reverts commit 6a8da39fb5c0dec4c0e46c6a24fc190361e28848.

--- Shipping_Cost_Calculator.py
+++ Shipping_Cost_Calculator.py
@@ -1,7 +1,6 @@
 # Shipping Cost Calculator
 
  # Here is a new update by <tony-s-h>
- # Here is another update by <tony-s-h>
 
 ## Input package weight and shipping rate
 weight = float(input("Enter the package weight in kilograms: "))

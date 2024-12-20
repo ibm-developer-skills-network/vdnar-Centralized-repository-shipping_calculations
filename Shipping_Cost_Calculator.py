f68c13c93dc57824668b76605da198ce4fa600ab
aeisolution-com <info@aeisolution.com>
2024-12-20 12:27:58 -0500

    Revert "added additional line to Shipping_Cost_Calculator.py"
    This reverts commit 2d2e045e8c4fb76e8c676dad0f6c364942b100c4.

--- Shipping_Cost_Calculator.py
+++ Shipping_Cost_Calculator.py
@@ -1,6 +1,5 @@
 # Shipping Cost Calculator
 # Here is a new update by aeisolution-com
-# Here is another update by aeisolution-com
 
 ## Input package weight and shipping rate
 weight = float(input("Enter the package weight in kilograms: "))

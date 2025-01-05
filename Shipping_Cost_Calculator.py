f91eba9883153afd9dd19acb2832f072b3616c67
Paras Patil <patilparas1105@gmail.com>
2025-01-05 14:19:34 -0500

    Added update comment with GitHub username

--- Shipping_Cost_Calculator.py
+++ Shipping_Cost_Calculator.py
@@ -10,3 +10,15 @@ shipping_cost = weight * rate
 ## Display the result
 print(f"Shipping Cost: {shipping_cost} USD")
 
+# Hera is a new update by paraspatil11
+
+
+
+
+
+
+
+
+                              [ Read 12 lines ]
+^G Help        ^O Write Out   ^W Where Is    ^K Cut         ^T Execute
+^X Exit        ^R Read File   ^\ Replace     ^U Paste       ^J Justify

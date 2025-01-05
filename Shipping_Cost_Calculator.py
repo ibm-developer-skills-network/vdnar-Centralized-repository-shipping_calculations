24a23e9dafa9125c126ceae482d384fa0d8ce9d6
Paras Patil <patilparas1105@gmail.com>
2025-01-05 14:33:40 -0500

    added additional line to Shipping_Cost_Calculator.py

--- Shipping_Cost_Calculator.py
+++ Shipping_Cost_Calculator.py
@@ -11,14 +11,3 @@ shipping_cost = weight * rate
 print(f"Shipping Cost: {shipping_cost} USD")
 
 # Hera is a new update by paraspatil11
-
-
-
-
-
-
-
-
-                              [ Read 12 lines ]
-^G Help        ^O Write Out   ^W Where Is    ^K Cut         ^T Execute
-^X Exit        ^R Read File   ^\ Replace     ^U Paste       ^J Justify

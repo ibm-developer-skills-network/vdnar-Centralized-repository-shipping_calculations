38c028ee44869efd3fc594f6fa86f8f312c0011e
Sergio Plascencia <153236644+DANPLAS@users.noreply.github.com>
2025-01-15 15:42:30 -0600

    Update Shipping_Cost_Calculator.py

--- Shipping_Cost_Calculator.py
+++ Shipping_Cost_Calculator.py
@@ -4,9 +4,19 @@
 weight = float(input("Enter the package weight in kilograms: "))
 rate = float(input("Enter the shipping rate per kilogram: "))
 
-## Calculate shipping cost
-shipping_cost = weight * rate
-
+##Extra charge for liquid shippping
+liquid = int(input("Does your shipment contain liquids? 1.-Yes 2.-No",))
+shipping_cost = weight*rate
+if liquid == 1:
+## Calculate extra shipping cost for liquid
+    shipping_cost = (weight * rate)*1.1
+    print("You have an extra charge for liquid shippment of 10%")
+    print("Your shipping cost is:", shipping_cost, "USD")
+elif liquid == 2:
 ## Display the result
-print(f"Shipping Cost: {shipping_cost} USD")
-
+    shipping_cost = weight * rate
+    print("Shipping Cost: USD", shipping_cost)
+else:
+    print("Incorrect option, we will suppose that the shippment does not include any liquids")
+    shipping_cost = (weight * rate)+10
+    print("Shipping Cost: USD", shipping_cost)

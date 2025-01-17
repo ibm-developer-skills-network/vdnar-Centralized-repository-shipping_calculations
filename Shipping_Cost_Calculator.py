58927c95b57a76538c868832f2a8581671436f55
datiraw <dati.raw@gmail.com>
2025-01-17 00:01:36 -0600

    changing the weight and rate of the shipping cost

--- Shipping_Cost_Calculator.py
+++ Shipping_Cost_Calculator.py
@@ -1,8 +1,8 @@
 # Shipping Cost Calculator
 
 ## Input package weight and shipping rate
-weight = float(input("Enter the package weight in kilograms: "))
-rate = float(input("Enter the shipping rate per kilogram: "))
+weight = float(input("50kg "))
+rate = float(input("$2/kg "))
 
 ## Calculate shipping cost
 shipping_cost = weight * rate
